85309081d518527d33a016a1762d630a9c08cd93
Daehyun Kim <hatemogi@gmail.com>
2021-04-23 00:25:05 +0900

    make it build on my mac

--- repl/build.sh
+++ repl/build.sh
@@ -22,8 +22,8 @@ fi
 ./node_modules/.bin/uglifyjs elm.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" \
   | ./node_modules/.bin/uglifyjs --mangle --output assets/repl.js
 
-echo "Initial size: $(wc elm.js -c) bytes"
-echo "Minified size:$(wc assets/repl.js -c) bytes"
+echo "Initial size: $(wc -c elm.js) bytes"
+echo "Minified size:$(wc -c assets/repl.js) bytes"
 echo "Gzipped size: $(gzip assets/repl.js -c | wc -c) bytes"
 
 rm elm.js
